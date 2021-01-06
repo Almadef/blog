1479b5ebcff9629b0a61ae94fbe1679e621ff394
almadef <kaberton@outlook.com>
2021-01-06 20:31:23 +0400

    [GH5.storage] add Swagger

--- src/modules/storage/storage.controller.ts
+++ src/modules/storage/storage.controller.ts
@@ -9,15 +9,30 @@ import {
   FileInterceptor,
   FileFieldsInterceptor,
 } from '@nestjs/platform-express';
+import {
+  ApiTags,
+  ApiConsumes,
+  ApiBody,
+  ApiCreatedResponse,
+  ApiBadRequestResponse,
+} from '@nestjs/swagger';
 import { StorageService } from './storage.service';
 import { BufferedFile } from './storage.models';
+import { FileUploadSingleDto, FileUploadManyDto } from './storage.dtos';
 
+@ApiTags('storage')
 @Controller('storage')
 export class StorageController {
   constructor(private storageService: StorageService) {}
 
   @Post('single')
   @UseInterceptors(FileInterceptor('image'))
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({
+    type: FileUploadSingleDto,
+  })
+  @ApiCreatedResponse({ description: 'Return link to get file.' })
+  @ApiBadRequestResponse({ description: 'Error save file on server.' })
   async uploadSingle(@UploadedFile() image: BufferedFile) {
     return await this.storageService.uploadSingle(image);
   }
@@ -29,6 +44,12 @@ export class StorageController {
       { name: 'image2', maxCount: 1 },
     ]),
   )
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({
+    type: FileUploadManyDto,
+  })
+  @ApiCreatedResponse({ description: 'Return links to get files.' })
+  @ApiBadRequestResponse({ description: 'Error save file on server.' })
   async uploadMany(@UploadedFiles() files: BufferedFile) {
     return this.storageService.uploadMany(files);
   }
